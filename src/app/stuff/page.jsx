344a1ca66b245f312b4978123317a72062b0b821
Levi Frank <LeviOrionFrank@gmail.com>
2023-06-21 14:47:04 -0400

    made homepage with text and link to /stuff, made the stuff page with text and link to homepage

---
+++ src/app/stuff/page.jsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <h1>
+      I'm the stuff page, go to the Home page through this link:
+      <br /> <br />
+      <a href="/">Link</a>
+    </h1>
+  );
+}
